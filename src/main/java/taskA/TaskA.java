3ffbc101079d39995665473691b64a1ebed16ede
CSEE Systems <csee-systems@surrey.ac.uk>
2024-03-27 00:28:56 +0000

    Initial commit

---
+++ src/main/java/taskA/TaskA.java
@@ -0,0 +1,16 @@
+package taskA;
+
+public class TaskA {
+
+    public static void main(String[] args) {
+
+	System.out.println("Operating Systems Coursework");
+	System.out.println("Name: "); // display your name in here
+	System.out.println("Please enter your commands - cat, cut, sort, uniq, wc or |");
+
+	// read the command from the terminal
+	// read the text file
+	String filePath = "taskA.txt";
+
+    }
+}
---
+++ src/main/java/taskC/TaskC.java
@@ -0,0 +1,11 @@
+package taskC;
+
+public class TaskC {
+
+    public static void main(String[] args) {
+
+	// Your code here.
+
+    }
+
+}
